f1c4e9f4e7166f7f20c300e1cb02f396da243f31
angel-zm <zamudiomartinezangel@gmail.com>
2025-11-21 13:03:00 -0600

    proyecto finalizado

---
+++ Proyecto_final/Galeria/Mesh_tn.cpp
@@ -0,0 +1,97 @@
+﻿#include "Mesh.h"
+
+Mesh::Mesh()
+{
+    VAO = 0;
+    VBO = 0;
+    IBO = 0;
+    indexCount = 0;
+}
+
+void Mesh::CreateMesh(GLfloat* vertices, unsigned int* indices,
+    unsigned int numOfVertices, unsigned int numOfIndices)
+{
+    indexCount = numOfIndices;
+
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    glGenBuffers(1, &IBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+        sizeof(indices[0]) * numOfIndices, indices, GL_STATIC_DRAW);
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER,
+        sizeof(vertices[0]) * numOfVertices, vertices, GL_STATIC_DRAW);
+
+    // layout: pos(3) + tex(2) + normal(3) = 8 floats
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(vertices[0]) * 8, 0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(vertices[0]) * 8,
+        (void*)(sizeof(vertices[0]) * 3));
+    glEnableVertexAttribArray(1);
+
+    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(vertices[0]) * 8,
+        (void*)(sizeof(vertices[0]) * 5));
+    glEnableVertexAttribArray(2);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
+
+void Mesh::RenderMesh()
+{
+    // si no hay VAO o no hay nada que dibujar, salimos
+    if (VAO == 0 || indexCount == 0)
+        return;
+
+    glBindVertexArray(VAO);
+
+    if (IBO != 0)
+    {
+        // caso normal: tenemos buffer de índices
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+        glDrawElements(GL_TRIANGLES, indexCount, GL_UNSIGNED_INT, 0);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    }
+    else
+    {
+        // caso modelos/meshes que solo tienen VBO (sin IBO)
+        glDrawArrays(GL_TRIANGLES, 0, indexCount);
+    }
+
+    glBindVertexArray(0);
+}
+
+
+void Mesh::ClearMesh()
+{
+    if (IBO != 0)
+    {
+        glDeleteBuffers(1, &IBO);
+        IBO = 0;
+    }
+
+    if (VBO != 0)
+    {
+        glDeleteBuffers(1, &VBO);
+        VBO = 0;
+    }
+
+    if (VAO != 0)
+    {
+        glDeleteVertexArrays(1, &VAO);
+        VAO = 0;
+    }
+
+    indexCount = 0;
+}
+
+Mesh::~Mesh()
+{
+    ClearMesh();
+}
